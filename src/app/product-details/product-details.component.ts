86c3cc385534e08d95c12cd48ae6aebb171e3521
ankitanandy <nandy.ankita@gmail.com>
2022-05-09 20:23:16 +0530

    Route to product details added

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
-
-  constructor() { }
+  product: Product;
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+    this.product = products.find((p) => p.id === productIdFromRoute);
   }
-
 }
