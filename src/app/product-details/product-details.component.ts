4199a88d85de5c56f110b5080a3f470579198652
ankitanandy <nandy.ankita@gmail.com>
2022-05-10 20:24:04 +0530

    cart component created

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -17,6 +17,7 @@ export class ProductDetailsComponent implements OnInit {
 
   addToCart(product: Product) {
     this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
   }
 
   ngOnInit() {
