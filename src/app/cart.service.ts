495acd4ba793c6107841078f53f3a459ab855e9b
ankitanandy <nandy.ankita@gmail.com>
2022-05-11 19:47:16 +0530

    implement http client

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 import { Product } from './products';
 
 @Injectable({
@@ -6,7 +8,7 @@ import { Product } from './products';
 })
 export class CartService {
   items: Product[] = [];
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
 
   addToCart(product: Product) {
     this.items.push(product);
@@ -19,5 +21,10 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices()
+  {
+    return this.httpClient.get<{type:string, price:number}[]>('/assets/shipping.json')
   }
 }
